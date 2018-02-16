d4ee65a853cb4db61d6bf7e3f94aa7e425ecf4ae
bLLekz <mr.trekill@mail.ru>
2018-02-16 15:22:10 +0300

    Feb 16

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,49 +24,60 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <!-- <ul class="navbar-nav mr-auto">
                         
-                    </ul>
+                    </ul> -->
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto word_stl">
-                        <li><a class="nav-link head_nav_stl" href="{{ url('/') }}"><i class="fa fa-search"></i> Поиск</a></li>
-                        <li><a class="nav-link head_nav_stl" href="{{ url('/') }}"><i class="fas fa-map-marker-alt"></i> Череповец <i class="fas fa-caret-down"></i></a></li>
+                        <li><div class="mr15"><a class="nav-link head_nav_stl nav_head" href="{{ url('/') }}"><i class="fa fa-search mr8 ico_size"></i> Поиск</a></div></li>
+                        <li><div class="mr15"><a class="nav-link head_nav_stl nav_head" href="{{ url('/') }}"><i class="fas fa-map-marker-alt mr8 ico_size"></i> Череповец <i class="fas fa-caret-down ml5"></i></a></div></li>
+                        <li><div class="mr15"><a class="nav-link head_nav_stl nav_head" href="{{ url('/') }}"><i class="fas fa-language mr8 ico_size"></i>
+                        Russian <i class="fas fa-caret-down ml5"></i></a></div></li>
 
                         <!-- Authentication Links --> 
                        <li class="nav-item dropdown"> 
                             <a class="nav-link dropdown-toggle drop_lk" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-user-circle"></i> Личный кабинет <span class="caret"></span>
+                                <i class="fas fa-user-circle mr8 ico_size"></i> Личный кабинет <span class="caret"></span>
                             </a>
                             <div class="dropdown-menu drop_stl word_stl" aria-labelledby="navbarDropdown">
-                                <div class="row">
-                                    <div class="col-md-8 col-md-ofset-2 top-cover text-center">
-                                    <span class="text-center word_stl3 lk_in">Вход</span>
-                                    <br>
-                                    <div class="fastin row">
-                                        <div class="col-md-6 top-cover text-center">
-                                            <span class="text-center">Быстрый вход</span>
+                                <div class="row justify-content-center text-center">
+                                    <div class="col-10">
+                                        <div class="row justify-content-end">
+                                            <div class="col-4">
+                                              <span class="text-center word_stl3 lk_in">Вход</span>
+                                            </div>
+                                            <div class="col-4  col align-self-start text-right">
+                                               <a href="#" class="times_stl">&times</a>
+                                            </div>
                                         </div>
-                                        <div class="col-md-2 top-cover text-center">
+                                    <div class="row justify-content-center mt10">
+                                        <div class="col-md-8">
+                                            <span class="fast_in">Быстрый вход</span>
+                                        </div>
+                                        <div class="col-md-2">
                                             <div class="bor_f">
                                                 <a href="/" class="soc_ico"><i class="fab fa-facebook-f"></i></a>
                                             </div>
                                         </div>
-                                        <div class="col-md-4 top-cover text-center">
+                                        <div class="col-md-2">
                                             <div class="bor_vk">
                                                 <a href="/" class="soc_ico"><i class="fab fa-vk"></i></a>
-                                         </div>   
+                                            </div>   
                                         </div>
                                     </div>
                                     <br>
-                                    <input class="form-control" type="text" placeholder="E-mail">
-                                    <input class="form-control" type="text" placeholder="Пароль">
-                                    <a href="/">Забыли пароль?</a>
-                                    <button class="btn btn-default form-control login_but">Войти</button>
-                                    <hr>
-                                    <a href="/">Зарегистрироваться <i class="fas fa-arrow-right"></i></a>
+                                    <input class="form-control login_inp" type="email" placeholder="E-mail">
+                                    <input class="form-control login_inp mt10" type="password" placeholder="Пароль">
+                                    <div class="fpass">
+                                        <a href="/" class="foget_pass">Забыли пароль?</a>
                                     </div>
+                                    <button class="btn btn-default form-control login_but">Войти</button>
                                 </div>
+                            </div>
+                            <div class="col-12 text-center">
+                                <hr>
+                                <a href="/" class="register_but">Зарегистрироваться <i class="fas fa-arrow-right"></i></a>
                             </div>
                         </li>
                             <!--  -->
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,12 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="contaner-fluid c">
+  <div class="container">
+    <div class="row r">
+      <div class="col-md-1 t"><img src="" alt=""></div>
+      <div class="col-md-3 t text-left">
+        <span>ПроВыборы</span>
+        <br>
+        <span>Сделай свой выбор правильно</span>
+      </div>
+      <div class="col-md t"></div>
+      <div class="col-md t">
+          <a href="/">Информация о выборах</a>
+      </div>
+      <div class="col-md t"> 
+          <a href="/">Политические партии</a>
+      </div>
+      <div class="col-md t">  
+          <a href="/">Где голосовать?</a>
+      </div>
+      <div class="col-md t">  
+          <a href="/">Частые вопросы</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- <div class="container c">
+  <div class="row justify-content-end">
+    <div class="col-4 t">
+      <span class="text-center word_stl3 lk_in">Вход</span>
+    </div>
+    <div class="col-4  col align-self-start text-right">
+      &times
+    </div>
+  </div>
+</div> -->
+
+<!-- <div class="container c">
+  <div class="row justify-content-around">
+    <div class="col-md t">1</div>
+    <div class="col-md t">2</div>
+    <div class="col-md t">3</div>
+  </div>
+</div> -->
+
+<!-- <div class="container c">
+  <div class="row ">
+    <div class="col-sm-2 t">1</div>
+    <div class="col-sm-8 mr-auto ml-auto t">2</div>
+    <div class="col-sm-2 t">3</div>
+  </div>
+</div>
+<br> -->
+
+<!-- 
 <div class="container c">
     <div class="row ">
         <div class="col-md t">1</div>
         <div class="col-md t">2</div>
         <div class="col-md t">3</div>
-        <div class="w-100"></div>  <!-- перенос на новую -->
+        <div class="w-100"></div>  <!-- перенос на новую 
         <div class="col-md t">4</div>
         <div class="col-md t">5</div>
         <div class="col-md t">6</div>
@@ -23,10 +77,12 @@
         <div class="col-md t">1</div>
         <div class="col-md t">2</div>
     </div>
+    <div class="row">
+        <div class="col-md t">1</div>
+    </div>
 </div>
 <br>
-
-<!-- <div class="container c">
+<div class="container c">
   <div class="row r align-items-end">
     <div class="col t">
       One of three columns
